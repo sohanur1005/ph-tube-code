65c33ac19f392366897fa18d8068df739db10c28
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-02 14:04:34 +0600

    send this

--- javascript/video.js
+++ javascript/video.js
@@ -37,6 +37,25 @@ const loadvideos=() =>
     .catch((error)=> console.log(error))
 }
 
+const cardDemo={
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
+
 const displayVideos=(videos)=>
 {
    const videoContainer=document.getElementById("videos")
@@ -47,17 +66,21 @@ const displayVideos=(videos)=>
      card.classList="card  card-compact"
      card.innerHTML= `
      
-     <figure>
+     <figure class="h-[200px]" >
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
+      src=${video.thumbnail}
+      class="h-full w-full object-cover "
+      alt="Shoes" >
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex">
+    <div>
+      <img class="h-10 w-10 rounded-full object-cover " src="${video.authors[0].profile_picture} ">
     </div>
+    <div>
+      <h2>/<h2>
+      <p></p>
+    </div>
+
   </div>
      
      `
