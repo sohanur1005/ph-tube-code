97e31b8ddff417e59cb552300566041e2a4fc177
sohanur1005 <sohanur.sr1005@gmail.com>
2026-02-02 11:08:59 +0600

    send this

--- javascript/video.js
+++ javascript/video.js
@@ -9,6 +9,20 @@ const loadCatagories=() =>
     .catch((error)=> console.log(error))
 }
 
+
+// load videos
+const loadvideos=() =>
+{
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res=> res.json())
+    .then(data => displayVideos(data.videos))
+    .catch((error)=> console.log(error))
+}
+
+const displayVideos=(videos)=>
+{
+   console.log(videos)
+}
 // create displayCategories
 const displayCategories =(categories)=>{
    const categoryContainer=document.getElementById('categories')
@@ -26,3 +40,4 @@ const displayCategories =(categories)=>{
 
 }
 loadCatagories();
+loadvideos();
